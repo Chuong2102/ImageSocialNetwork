1b4cfda4cde039b49c2c4558aee0686dc7950677
Chuong2102 <39906259+Chuong2102@users.noreply.github.com>
2023-03-27 17:22:55 +0700

    27-3

--- ImageSocialNetwork/Controllers/UpLoadImageController.cs
+++ ImageSocialNetwork/Controllers/UpLoadImageController.cs
@@ -6,13 +6,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using ImageSocialNetwork.Infrastructure.Configurations;
 using ImageSocialNetwork.Infrastructure.EF;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace ImageSocialNetwork.Controllers
 {
-    [Route("api/UpLoadImage")]
     [ApiController]
     public class UpLoadImageController : ControllerBase
     {
-       
+        IWebHostEnvironment environment;
+        public UpLoadImageController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        [HttpPost]
+        [Route("api/Upload")]
+        public async Task<string> Upload(IFormFile file)
+        {
+            if(file.Length > 0)
+            {
+                try
+                {
+                    DirectoryInfo directoryInfo;
+
+                    if (!Directory.Exists(environment.WebRootPath + "\\Images\\"))
+                    {
+                        directoryInfo = Directory.CreateDirectory(environment.WebRootPath + "\\Images\\");
+                    }
+
+                    FileStream fileStream = System.IO.File.Create(environment.WebRootPath + "\\Images\\" + file.FileName);
+                    await file.CopyToAsync(fileStream);
+                    fileStream.Flush();
+
+                    return environment.WebRootPath + "\\Images\\" + file.FileName;
+
+                    
+                }
+                catch(Exception ex)
+                {
+                    return ex.ToString();
+                }
+
+            }
+
+            return "Not success";
+
+        }
     }
 }
